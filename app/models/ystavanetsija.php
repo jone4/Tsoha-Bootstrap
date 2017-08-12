bab694632222cc073b4c0dc5a00d33676c67f531
Johanna <johanna.tuhkanen@helsinki.fi>
2017-08-12 19:04:06 +0300

    ystavanetsija-malli ja ystavanetsijat-näkymä tehty. composer ja reitit muokattu. :)

---
+++ app/models/ystavanetsija.php
@@ -0,0 +1,53 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Ystavanetsija extends BaseModel{
+ public $id, $kayttaja_id, $nimi, $julkaistu, $status, $kuvaus;  
+
+public function __construct($attribuutit){
+    parent::__construct($attribuutit);
+}
+public static function kaikki(){
+    $jono = DB::connection()-> prepare('SELECT * FROM Ystavanetsija');
+    $jono-> execute();
+    $rivit = $jono->fetchAll();
+    $ystavanetsijat=array();
+    
+    foreach ($rivit as $rivi){
+        $ystavanetsijat[] = new Ystavanetsija (array(
+                'id'=> $rivi['id'],
+                'kayttaja_id'=> $rivi['kayttaja_id'],
+                    'nimi'=> $rivi['nimi'],
+                'julkaistu'=> $rivi['julkaistu'],
+                'status'=> $rivi['status'],
+                'kuvaus'=> $rivi['kuvaus']
+                
+                ));
+    }
+ return $ystavanetsijat;   
+}
+
+public static function etsi($id){
+    $jono = DB::connection()-> prepare ('SELECT * FROM Ystavanetsija WHERE id = :id LIMIT 1');
+    $jono-> execute (array('id'=> $id));
+    $rivi = $jono-> fetch();
+    
+    if($rivi){
+        $ystavanetsija = new Ystavanetsija(array(
+             'id'=> $rivi['id'],
+                'kayttaja_id'=> $rivi['kayttaja_id'],
+                    'nimi'=> $rivi['nimi'],
+                'julkaistu'=> $rivi['julkaistu'],
+                'status'=> $rivi['status'],
+                'kuvaus'=> $rivi['kuvaus']   
+                ));
+        return $ystavanetsija;
+}
+return null;
+}
+}
--- config/routes.php
+++ config/routes.php
@@ -8,13 +8,22 @@
     HelloWorldController::sandbox();
   });
 
-  $routes-> get("/people", function(){
-  HelloWorldController::people_list();
+  //$routes-> get("/people", function(){
+  //HelloWorldController::people_list();
+  //});
+  
+   $routes-> get("/people",function(){
+  YstavanetsijaController::index();
   });
   
   $routes-> get ("/people/aatumikkonen", function(){
       HelloWorldController::person_show();
   });
+  
+  $routes-> get ("/people/:id", function($id){
+      YstavanetsijaController::show($id);
+  });
+  
   
     $routes-> get ("/muokkaa", function(){
       HelloWorldController::muokkaa_profiilitietojani();
